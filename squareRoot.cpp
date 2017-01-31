89fd03396b808af1fa42b08c45dea9bd55a70a66
Shankar Kulumani <shanks.k@gmail.com>
2017-01-30 20:10:29 -0500

    Add example of gtest

---
+++ squareRoot.cpp
@@ -0,0 +1,15 @@
+// simple square root function
+#include <math.h>
+
+double squareRoot(const double a)
+{
+    double b = sqrt(a);
+    if (b != b)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return sqrt(a);
+    }
+}
